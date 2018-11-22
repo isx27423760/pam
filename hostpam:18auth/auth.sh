e3abf43f91c1c6aa312e8963eaa4bcf81c5fdc01
root <root@i16.informatica.escoladeltreball.org>
2018-11-22 09:15:54 +0100

    mes canvis

--- hostpam:18auth/auth.sh
+++ hostpam:18auth/auth.sh
@@ -2,5 +2,5 @@
 # @edt ASIX-M06
 #descripcio: scipt per generar automaticament la connexio amb usaris del servidor ldap
 # ---------------------------------------------------------------------------------
-authconfig  --enableshadow --enablelocauthorize --enableldap  --ldapserver='ldap' --ldapbase='dc=escoladeltreball,dc=org' --enableldapauth  --updateall
+authconfig  --enableshadow --enablelocauthorize --enableldap  --ldapserver='ldap' --ldapbase='dc=edt,dc=org' --enableldapauth  --updateall
 
