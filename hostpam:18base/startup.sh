320b1997025f3f8856e59a72be40c54aca4616b3
isx27423760 <isx27423760@i16.informatica.escoladeltreball.org>
2018-12-20 08:58:27 +0100

    v02

--- hostpam:18base/startup.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-# @edt ASIX M06 2018-2019
-# startup.sh hostpam
-#--------------------------------------
-
-/opt/docker/install.sh && echo "Install OK"
-/usr/sbin/nslcd && echo "nslcd Ok"
-/usr/sbin/nscd && echo "nscd Ok"
-/bin/bash
